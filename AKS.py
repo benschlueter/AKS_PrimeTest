8cfb9a21981ce860d0f2e95b739d93ccb17c8ec7
Kakashiiiiy <benedictschlueter@gmail.com>
2019-03-13 14:26:39 +0100

    Update AKS.py
    Fix phi(n)

--- AKS.py
+++ AKS.py
@@ -16,18 +16,18 @@ def phi(n):
     for k in range(1, n + 1):
         if math.gcd(k, n) == 1:
             amount += 1
-        return amount
+    return amount
 
 def step1(n):
     for b in range(2,int(math.log2(n)+1)):
         a = n**(1/b)
         if math.floor(a) == a:
+            #print("[-]"+str(n)+" is no Prime 1")
             return False
     return True
 
 def step2(n):
     mk = math.log2(n)**2
-    mr = math.log2(n)**5
     nexr = True
     r = 1
     while nexr == True:
@@ -39,12 +39,13 @@ def step2(n):
             if exp_func(n,k,r) == 0 or exp_func(n,k,r) == 1:
                 nexr = True
     return r
+        
 
 def step3 (n, r):
-    for a in range(r, 2):
-        if 1 < math.gcd(a, n) < n:
+    for a in range(1,r+1):
+        if ((1 < math.gcd(a, n)) & (math.gcd(a,n) < n)):
+            #print("[-]"+str(n)+" is no Prime 3")
             return False
-
 
 def step4(n, r):
     if n > 5690034:
@@ -54,27 +55,32 @@ def step4(n, r):
 
 
 def step5(n, r):
-    x = 8
+    x = 7
     max = math.sqrt(phi(r))
-    for a in range(1, math.floor(max*math.log2(n))):
+    rn = math.floor(max*math.log2(n))
+    cache = exp_func(x,n,n)
+    for a in range(1, rn+1):
         b = exp_func((x+a),n,n) #((x + a) ** n) % n
-        l = (exp_func(x,n,n)+a)%n #(x ** n + a) % n
+        l = (cache+a)%n #(x ** n + a) % n
         if b != l:
+            #print("[-]"+str(n)+" is no Prime 5")
             return False
     print("[+]"+str(n)+" is a Prime Number Step 5")
     return True
 
 
 def aks(n):
+    #print("Testing Number: "+str(n))
     if step1(n) == True:
-        r = step2(n)
+        #r = step2(n)
+        r = 5843
         if step3(n, r) != False:
             if step4(n, r) != True:
                 step5(n, r)
 
 
-#for i in range(2,10000):
-#   aks(i)
 
-print("lastNum")
-aks(101234567897)
+for i in range(2,1000):
+   aks(i)
+#print(100207100213100237100267*100207100213100237100267)
+#aks(671998030559713968361666935769)
